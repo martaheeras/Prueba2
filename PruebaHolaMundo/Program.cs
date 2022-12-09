c290a2b10ad6efba978bcf7c22e3e9860bca8202
LAPTOP-SPQIVFC1\marta <martaherasalbares@gmail.com>
2022-12-09 18:13:01 +0100

    Cambios en Visual

--- PruebaHolaMundo/Program.cs
+++ PruebaHolaMundo/Program.cs
@@ -6,7 +6,7 @@ namespace PruebaHolaMundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
         }
     }
 }
