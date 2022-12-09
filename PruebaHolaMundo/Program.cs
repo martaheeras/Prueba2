e6fa2f0853bc9cde5f4fa6055836c262f0587f11
LAPTOP-SPQIVFC1\marta <martaherasalbares@gmail.com>
2022-12-09 18:00:59 +0100

    Nuevo Visual Studio

---
+++ PruebaHolaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PruebaHolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
